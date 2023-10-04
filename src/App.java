73acb47c495af7606ed685500da300d39d517341
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-10-04 10:57:45 +0200

    la till kod som jämför tal 1 och 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.print("Skriv ett heltal: ");
+        int tal1 = tangentbord.nextInt();
+        tangentbord.nextLine();
+        System.out.print("Skriv ett till heltal: ");
+        int tal2 = tangentbord.nextInt();
+        tangentbord.nextLine();
+        System.out.println("Är tal1 och tal2 samma tal?: "+(tal1==tal2));
+        tangentbord.close();
     }
 }
